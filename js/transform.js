c79fb3eadccf0e7b11e80771c0e58f556dc7f9e0
Fsesnick <fernanda_sesnick@hotmail.com>
2020-12-28 12:25:11 +0000

    capture input

---
+++ js/transform.js
@@ -0,0 +1,5 @@
+const passphrase = 'I have Spoken';
+console.log( passphrase.length);
+console.log("This is my string".length);
+//console.log(passphrase.toLowerCase());
+console.log(passphrase.toUpperCase());

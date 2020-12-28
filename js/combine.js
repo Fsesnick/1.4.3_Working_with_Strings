478a1cf19160907777c3a363cf236603b164204a
Fsesnick <fernanda_sesnick@hotmail.com>
2020-12-28 14:14:25 +0000

    template literals

---
+++ js/combine.js
@@ -0,0 +1,4 @@
+const name = prompt("What is your name?");
+const message= "Hello "+ name+ ".Welcome to my music site";
+
+console.log(message);
--- js/template-literals.js
+++ js/template-literals.js
@@ -1,5 +1,8 @@
 const name = prompt('What is your name?');
 
-const message = "Hello" + name;
+//const message = `Hello, ${name}. It's ${2*3} o'clock`;
+
+
+const message = `Hello, ${name}. Welcome to my music site. I'm happy that you came by to visit, ${name}.`;
 
 console.log(message);
